7381a206c0504cc8d3d65faca8ea1a7815a682fd
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-04-15 18:09:46 +0200

    add feature test config  pns-isa-devops/projet-isa-devops-20-team-b-20#75

--- integration-tests/src/test/java/integration/RunCucumberTest.java
+++ integration-tests/src/test/java/integration/RunCucumberTest.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty" })
+@CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/features")
 public class RunCucumberTest {
 }

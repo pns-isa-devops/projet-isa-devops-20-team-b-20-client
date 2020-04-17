33a58e2b9b4a3f9ec7dc3f385ccf82697b617959
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-04-17 14:10:42 +0200

    dependencies in parent pom pns-isa-devops/projet-isa-devops-20-team-b-20#78

--- integration-tests/src/test/java/integration/RunCucumberTest.java
+++ integration-tests/src/test/java/integration/RunCucumberTest.java
@@ -1,8 +1,9 @@
 package integration;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
 import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty" }, features = "src/test/resources/features")

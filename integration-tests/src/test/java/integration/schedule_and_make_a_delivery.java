d4ccf72cb37b946ac706a98bcda3615f1beddd25
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-04-17 17:15:06 +0200

    fix packages pns-isa-devops/projet-isa-devops-20-team-b-20#87

--- integration-tests/src/test/java/integration/schedule_and_make_a_delivery.java
+++ integration-tests/src/test/java/integration/schedule_and_make_a_delivery.java
@@ -1,29 +1,20 @@
 package integration;
 
-import io.cucumber.java.Before;
+import client.office.api.DeliveryScheduleAPI;
+import client.office.framework.ShellOffice;
+import client.utils.cli.commands.Command;
+import client.utils.cli.framework.Shell;
+import client.warehouse.api.DeliveryAPI;
+import client.warehouse.api.DroneMaintenanceAPI;
+import client.warehouse.cli.commands.Adddrone;
+import client.warehouse.framework.ShellWarehouse;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import static org.junit.Assert.*;
-
-import org.junit.Ignore;
-
-import api.DeliveryAPI;
-import api.DeliveryScheduleAPI;
-import api.DroneMaintenanceAPI;
-import cli.commands.Adddrone;
-import cli.commands.Bye;
-import cli.commands.Command;
-import cli.commands.Help;
-import cli.commands.ScheduleDelivery;
-import cli.framework.Shell;
-import framework.ShellOffice;
-import framework.ShellWarehouse;
-
 public class schedule_and_make_a_delivery {
     private static String host = "localhost";
-    private static String port = "88080";
+    private static String port = "8080";
 
     Shell shellOffice;
     Shell shellWarehouse;
@@ -35,9 +26,28 @@ public class schedule_and_make_a_delivery {
 
     @Given("a drone {string} added in the system")
     public void a_drone_is_added_in_the_system(String id) throws IllegalAccessException, InstantiationException {
-        // setup();
-        // Command c = new Adddrone();
-        // c.setShell(shellWarehouse);
-        // c.execute(null);
+        setup();
+        Command c = new Adddrone();
+        c.setShell(shellWarehouse);
+        c.execute(null);
     }
+
+    @Given("a parcel {string}")
+    public void a_parcel(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I schedule a delivery at {string}")
+    public void i_schedule_a_delivery_at(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I can start the delivery")
+    public void i_can_start_the_delivery() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
 }

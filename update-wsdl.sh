2f662aed675f0a7eb7d427678a17e26c26cb1073
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-05-03 14:33:35 +0200

    fix sh pns-isa-devops/projet-isa-devops-20-team-b-20#119

--- update-wsdl.sh
+++ update-wsdl.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
 echo "# Retrieving the new WSDL file from the web service"
 echo ""
 echo "Source project : "
